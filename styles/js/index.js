0602c47882268b1e1564670ba02e0b729236a678
h-w-h-p <hello@h-w-h-p.digital>
2019-05-24 16:47:25 +0300

    Update
    Add validation

--- styles/js/index.js
+++ styles/js/index.js
@@ -1,30 +1,76 @@
 $('document').ready(function() {
 
-//****************************   Navigation   ****************************//
-
-	function mobile_gallery(){
-		$('.b-middle__form .form-item label input').click(function(){
-			if ($(this).is(':checked')) {
-				$('.b-middle__form .form-item figure').css({"display":"none"});
-				$(this).prev().css({"display":"block"});	
-				console.log('1');
-			} else{
-				$(this).prev().css({"display":"none"});
-
-			}
-			
-		});	
-	}
-
-	if ($(window).width() < 1023) {
-			mobile_gallery();
-		} else{
-			$('.b-middle__form .form-item figure').css({"display":"block"});
-		}
-
 	
+//
+$('.b-middle__form.form-2 dl dd .big-btn').click(function () {
+    var trigger = true
+    $(this).closest('form').find('.validate').each(function (i) {
+      var _this = this
+      if (!validate(_this, trigger)) {
+        $(this).closest('.b-middle__form.form-2 dl dd').addClass('danger')
+        trigger = false
+      }
+    })
+    if (!trigger) return false
+  })
 
-	
+  function validate (_this, trigger) {
+    var ckName = /^[А-Яа-яA-Za-z\s]{1,20}$/
+    var ckText = /^[А-Яа-яA-Za-z0-9,.!?\s]{1,5000}$/
+    var ckTel = /\(?([0-9]{3})\)?([ .-]?)([0-9]{3})\2([0-9]{4})/
+    var ckNumber = /^\d+$/
+    var ckDate = /^(\d{1,2}).(\d{1,2}).(\d{2}|\d{4})$/
+    var ckEmail = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i
+
+    var type = $(_this).attr('type')
+    // console.log(type)
+    if (type === 'number') {
+      if (!ckNumber.test($(_this).val())) {
+        return false
+      } else {
+        return true
+      }
+    } if (type === 'text') {
+      if (!ckText.test($(_this).val())) {
+        return false
+      } else {
+        return true
+      }
+    } if (type === 'password') {
+      if (!ckText.test($(_this).val())) {
+        return false
+      } else {
+        return true
+      }
+    } if (type === 'date') {
+      console.log('date')
+      if (!ckDate.test($(_this).val())) {
+        return false
+      } else {
+        return true
+      }
+    } if (type === 'email') {
+      if (!ckEmail.test($(_this).val())) {
+        return false
+      } else {
+        return true
+      }
+    } if (type === 'tel') {
+      if (!ckTel.test($(_this).val())) {
+        return false
+      } else {
+        return true
+      }
+    } if (type === 'name') {
+      if (!ckName.test($(_this).val())) {
+        return false
+      } else {
+        return true
+      }
+    } else {
+      return true
+    }
+  }	
 	
 
 
